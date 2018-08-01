e3dcecbed56563d3b069618457d927f5b1b088de
txniaa <txniaa@gmail.com>
2018-08-01 13:30:11 -0400

    adding my files

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",function(){
+  window.alert("CLOSE THIS AND THEN PRESS ANY KEY")
+});
+
+window.addEventListener('keydown', function(e) {
+  window.alert("Damn you really just listened to a text box.")
+});
